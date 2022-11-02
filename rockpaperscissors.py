e1e74dce761b74775d1eb36d9ddd090f3e5887fa
zebra-spots <88425510+zebra-spots@users.noreply.github.com>
2022-11-02 07:18:05 +0000

    Update rockpaperscissors.py
    PEP8 changes

--- rockpaperscissors.py
+++ rockpaperscissors.py
@@ -1,5 +1,6 @@
 import random
 
+
 def play():
     user = input("'r' for rock, 'p' for paper, 's' for scissors\n")
     computer = random.choice(['r', 'p', 's'])
@@ -19,5 +20,3 @@ def is_win(player, opponent):
     if (player == 'r' and opponent == 's') or (player == 's' and opponent == 'p') \
             or (player == 'p' and opponent == 'r'):
         return True
-
-
