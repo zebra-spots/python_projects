73d49e13a43d22c82853e56878498e24fd465356
zebra-spots <88425510+zebra-spots@users.noreply.github.com>
2022-11-02 07:10:53 +0000

    Add files via upload

---
+++ MyGame.py
@@ -0,0 +1,40 @@
+#! /usr/bin/python
+# Name: MyGame.py
+# Author: Chris
+# Revision v1.0
+# Description: This is a testing program.
+"""
+    Docstring: Use to play games. Import rock paper scissors game.
+    TODO: create and import more games
+"""
+import sys
+from rockpaperscissors import *
+
+
+def main():
+    """ Call games here """
+    print("Which game?\n"
+          "1: rockpaperscissors\n"
+          "2: Test statement\n")
+    userchoice = input("Make a choice: ")
+
+    if userchoice == "1":
+
+        while True:
+            print(play())
+            userin = input("Press n to quit, Or press any key to play again ")
+            if userin.lower() == "n":
+                print("Quitting.......")
+                break
+
+    elif userchoice == "2":
+        print("Test")
+
+    else:
+        print("Quitting.......")
+
+    return None
+
+if __name__ == "__main__":
+    main()
+    sys.exit(0)
---
+++ rockpaperscissors.py
@@ -0,0 +1,23 @@
+import random
+
+def play():
+    user = input("'r' for rock, 'p' for paper, 's' for scissors\n")
+    computer = random.choice(['r', 'p', 's'])
+
+    if user == computer:
+        return "Draw"
+
+    if is_win(user, computer):
+        return "You won"
+
+    return "You lost"
+
+    # r > s, s > p, p > r
+
+
+def is_win(player, opponent):
+    if (player == 'r' and opponent == 's') or (player == 's' and opponent == 'p') \
+            or (player == 'p' and opponent == 'r'):
+        return True
+
+
